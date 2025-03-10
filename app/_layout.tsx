961a000ca1f384c7c02bff68b3794cb55abe7b20
ossian igelberg <ossian.igelberg@gmail.com>
2025-03-10 14:06:09 +0100

    switched back to react navigation, removed ui-kitten

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,5 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome';
-import * as eva from '@eva-design/eva';
-import { ApplicationProvider } from '@ui-kitten/components';
-import { DefaultTheme, DarkTheme } from '@react-navigation/native';
-
+import { DefaultTheme, DarkTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
@@ -50,19 +47,15 @@ export default function RootLayout() {
 
 function RootLayoutNav() {
   const colorScheme = useColorScheme(); 
-  const theme = colorScheme === 'dark' ? eva.dark : eva.light;
 
   return (
-    <ApplicationProvider {...eva} theme={theme}>
-      <Stack screenOptions={{
-          headerStyle: {
-            backgroundColor: colorScheme === 'dark' ? '#000' : '#fff', // Navbar color
-          },
-          headerTintColor: colorScheme === 'dark' ? '#fff' : '#000', // Navbar text color
-        }}>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
-      </Stack>
-    </ApplicationProvider>
+    
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
+    </Stack>
+  </ThemeProvider>
+    
   );
 }
